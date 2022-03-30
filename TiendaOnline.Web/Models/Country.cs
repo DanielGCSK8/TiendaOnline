280b9c00aa175cedd85e5cd381696f99d8cc03c9
Danielrvrt <39166172+Danielrvrt@users.noreply.github.com>
2022-03-30 09:54:57 -0500

    Crud countries, models department y city

---
+++ TiendaOnline.Web/Models/Country.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TiendaOnline.Web.Models
+{
+    public class Country
+    {
+
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(30,ErrorMessage ="El campo {0} debe contener al menos un caracter")]
+        public string Name { get; set; }
+
+        public ICollection<Department> Departments { get; set; }
+        [DisplayName("Departments Number")]
+        public int DepartmentsNumber => Departments == null ? 0 : Departments.Count;
+
+    }
+}
